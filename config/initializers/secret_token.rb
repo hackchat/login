9ebb0597850ceb970c00b06edd6209a70ba9aa40
tkiefhaber <tom.kiefhaber@hungrymachine.com>
2012-05-30 15:45:52 -0400

    we made an app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackchatAuth::Application.config.secret_token = '0dce1341585a60f07b1756ef3a9b6a47efcd29a4635e4e9150f18f9925d2a50d847323055e3d94e67193d28f1a29363057785d6afbab66985909b17b567e25b6'
