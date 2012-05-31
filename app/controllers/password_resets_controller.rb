40dbe46c83a47f8fab8eb3705ec7f27c43a98c2e
tkiefhaber <tom.kiefhaber@hungrymachine.com>
2012-05-31 14:18:46 -0400

    asdfasdf

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,7 +2,7 @@ class PasswordResetsController < ApplicationController
   def create
     @user = User.find_by_email(params[:email])
     @user.deliver_reset_password_instructions! if @user
-    redirect_to(root_path, :notice = t(:password_instructions))
+    redirect_to(root_path, :notice => t(:password_instructions))
   end
 
   def edit
