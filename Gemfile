87b3e3e0336cc41b927377c0b3f285157a1cf40d
tkiefhaber <tom.kiefhaber@hungrymachine.com>
2012-06-07 10:08:52 -0400

    committing for fun

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ end
 
 gem 'jquery-rails'
 gem 'client_side_validations'
+gem 'twitter-bootstrap-rails'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,10 @@
-root = "/home/deployer/apps/hackchat_auth/current"
+root = "/home/deployer/apps/login/current/public"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.hackchat_auth.sock"
+listen "/tmp/unicorn.login.sock"
+listen 7004
 worker_processes 2
 timeout 30
