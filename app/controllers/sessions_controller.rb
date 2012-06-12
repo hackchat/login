9af00c5fda52d919ca3904e6b791599d41bfdbbc
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-06-12 10:32:07 -0400

    adding more loggers!

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,11 +10,13 @@ class SessionsController < ApplicationController
   end
 
   def new
+    Rails.logger.error session.inspect
+    Rails.logger.error current_user.inspect
     redirect_to redirect_chat if current_user
   end
 
   def create
-    @user = login(params[:email], params[:password], params[:remember_me])
+    @user = login(params[:email], params[:password], remember_me = false)
     if @user
       session[:user_token] = @user.auth_token
       redirect_to redirect_chat, flash[:notice] => "signed in!"
