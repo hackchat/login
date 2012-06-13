45412e149eaa13a0d91b5573a94b60738715c06d
Melanie Gilman <melanie.gilman@gmail.com>
2012-06-13 18:02:43 -0400

    add user lookup by email

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,8 +1,12 @@
 class EmailsController < ActionController::Base
 
   def show
-    @user = User.find_by_email(params[:id])
-    render status: :bad_request unless @user
+    email = Base64.decode64(params[:id])
+    if user = User.find_by_email(email)
+      render :json => {user: {email: user.email, user_token: user.user_token}}
+    else
+      head status: :not_found
+    end
   end
 
 end
